beda66b483aa8a25e3479ac891466e819a3b646c
Pavel Sukhodolskii <pavel.sukhodolski@microavia.com>
2024-12-12 13:48:07 +0400

    fix deps for cleanup improve docker with app to nginx add caching and compression on nginx side

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,20 @@ export default function Document() {
                 <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
                 <link rel="manifest" href="favicon/site.webmanifest"/>
 
+                <meta name="description"
+                      content="CV of Pavel Sukhodolskii, a Senior Frontend Developer based in Dubai with extensive experience in JavaScript, TypeScript, React, WebGL, WebRTC, Docker, and Go. Contributing to OpenGlobus and creating React component kits. Over 10 years in frontend development and 4 years in the drone industry."/>
+                <meta name="keywords"
+                      content="Pavel Sukhodolskii, Senior Frontend Developer, Frontend Engineer, JavaScript, TypeScript, React, WebGL, WebRTC, Docker, Go, Drone Industry, GIS, OpenGlobus, Dubai"/>
+                <meta name="author" content="Pavel Sukhodolskii"/>
+                <meta name="robots" content="index, follow"/>
+
+                <meta property="og:title" content="Pavel Sukhodolskii - Senior Frontend Developer"/>
+                <meta property="og:description"
+                      content="Senior Frontend Developer with over 10 years of experience. Skilled in JavaScript, TypeScript, React, WebGL, WebRTC, Docker, and Go. Contributing to OpenGlobus and creating React component kits."/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://example.com/cv/pavel-sukhodolskii"/>
+                <meta property="og:image" content="https://example.com/cv/profile.jpg"/>
+                <meta property="og:site_name" content="Pavel Sukhodolskii CV"/>
             </Head>
             <title>CV Pavel Sukhodolskii</title>
             <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96"/>
